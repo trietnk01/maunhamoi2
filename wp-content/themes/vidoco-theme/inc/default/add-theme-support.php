42fb95d6bbab5ee117bea28b58a5e99041d45663
dienkim <trietnk01@gmail.com>
2019-03-26 10:31:46 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
+++ wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
@@ -6,12 +6,8 @@ add_action('init','p_load_support');
 function p_load_support(){
     register_nav_menus(
         array(
-            'primary'    => __( 'Primay Menu', TDM  ),
-            'mobile'  => __( 'Mobile Menu', TDM  ),
-            'cateproduct'  => __( 'Danh mục sản phẩm', TDM  ),
-            'intromenu'    => __( 'Giới thiệu', TDM  ),
-            'productmenuleft'    => __( 'Sản phẩm trái', TDM  ),
-            'productmenuright'    => __( 'Sản phẩm phải', TDM  ),
+            'primary'    => __( 'Primay Menu'),
+            'mobile'  => __( 'Mobile Menu'),            
         )
     );
 
