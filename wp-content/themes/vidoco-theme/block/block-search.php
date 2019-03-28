5788a707b011ecf1b42613715992551880d6d1e5
dienkim <trietnk01@gmail.com>
2019-03-28 14:58:05 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/block/block-search.php
+++ wp-content/themes/vidoco-theme/block/block-search.php
@@ -3,12 +3,12 @@
 		<h3 class="h-total-right-tieu-de">Tìm kiếm</h3>
 		<div class="clr"></div>
 	</div>
-	<form class="frm-search-article margin-top-5" name="frm-search" method="POST" action="">
+	<form class="frm-search-article margin-top-5" name="frm_search" method="POST" action="">
 		<div class="frm-search-left">
 			<input value="<?php echo @$q; ?>" name="s" type="search" placeholder="Từ cần tìm..." class="input-box" autocomplete="off" >
 		</div>								
 		<div class="frm-search-right">
-			<a href="javascript:void(0);" onclick="document.forms['frm-search'].submit();">Tìm</a>
+			<a href="javascript:void(0);" onclick="document.forms['frm_search'].submit();">Tìm</a>
 		</div>										
 	</form>
 </div>
