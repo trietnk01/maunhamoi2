713c1ac54a50056c4cc7491149690bfe540270cb
dienkim <trietnk01@gmail.com>
2019-03-28 15:33:25 +0700

    cập nhật

---
+++ wp-content/themes/vidoco-theme/block/block-ban-quan-tam.php
@@ -0,0 +1,17 @@
+<div class="total-right co-the-ban-se-quan-tam-block">
+	<div class="h-total-right">
+		<h3 class="h-total-right-tieu-de">Có thể bạn sẽ quan tâm</h3>
+		<div class="clr"></div>
+	</div>
+	<div class="co-the-ban-se-quan-tam-box">
+		<ul>
+			<?php 
+			for ($i=1; $i <= 10 ; $i++) { 
+				?>
+				<li><a href="javascript:void(0);">Biệt thự tân cổ điển</a></li>
+				<?php
+			}
+			?>			
+		</ul>
+	</div>
+</div>
