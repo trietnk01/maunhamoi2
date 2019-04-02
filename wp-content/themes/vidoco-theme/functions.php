a510a7d7ef5c21e23cca9f759e701d324ab9a40e
dienkim <trietnk01@gmail.com>
2019-04-02 12:13:29 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/functions.php
+++ wp-content/themes/vidoco-theme/functions.php
@@ -92,3 +92,26 @@ function str_slug( $filename ) {
     return $sanitized_filename;
 }
 /* end str_slug */
+/* begin comment */
+function bcd_comment($comment, $args, $depth)    {
+    $GLOBALS['comment'] = $comment; ?>
+    <li <?php comment_class();?> id="li-comment-<?php comment_ID();?>">
+     
+        <div id="comment-<?php comment_ID();?>" class="clearfix comment-body">
+            <div class="comment-author vcard">
+                <?php echo get_avatar($comment, $size='60', $default='<path_to_url>'); ?>
+                <?php printf(__('<span class="fn">%s</span><br />'), get_comment_author_link()); ?>
+                <?php if($comment->comment_approved == '0') : ?>
+                <em><?php echo 'Your coment is waiting for moderation.';?></em>
+                <?php endif; ?>
+            </div><!-- end comment autho vcard-->
+            <div class="comment-meta commentmetadata">
+            <?php printf(get_comment_date());?><?php edit_comment_link(__('(Edit)'),' ',''); ?>
+            </div><!--end .comment-meta-->
+            <p class="commentcontent"><?php comment_text(); ?></p>
+            <div class="reply">
+                <?php comment_reply_link(array_merge($args,array('depth' => $depth, 'max_depth'=> $args['max_depth'])));?>
+            </div><!--end .reply-->
+        </div><!--end #comment-author-vcard-->
+<?php }
+/* end comment */
--- wp-content/themes/vidoco-theme/template-product-detail.php
+++ wp-content/themes/vidoco-theme/template-product-detail.php
@@ -35,16 +35,16 @@ if(have_posts()){
 								wp_reset_postdata();
 							}
 							?>
-						</div>
+						</div>						
 					</div>
 					<div class="col-lg-4">
 						<?php include get_template_directory()."/block/block-search.php"; ?>
-							<?php include get_template_directory()."/block/block-fanpage.php"; ?>
-							<?php include get_template_directory()."/block/block-video.php"; ?>
-							<?php include get_template_directory()."/block/block-mau-thiet-ke.php"; ?>														
-							<?php include get_template_directory()."/block/block-y-kien-khach-hang.php"; ?>
-							<?php include get_template_directory()."/block/block-ban-quan-tam.php"; ?>
-							<?php include get_template_directory()."/block/block-visitor-counter.php"; ?>
+						<?php include get_template_directory()."/block/block-fanpage.php"; ?>
+						<?php include get_template_directory()."/block/block-video.php"; ?>
+						<?php include get_template_directory()."/block/block-mau-thiet-ke.php"; ?>														
+						<?php include get_template_directory()."/block/block-y-kien-khach-hang.php"; ?>
+						<?php include get_template_directory()."/block/block-ban-quan-tam.php"; ?>
+						<?php include get_template_directory()."/block/block-visitor-counter.php"; ?>
 					</div>
 				</div>
 				<?php include get_template_directory()."/block/block-menu-content-bottom.php"; ?>
