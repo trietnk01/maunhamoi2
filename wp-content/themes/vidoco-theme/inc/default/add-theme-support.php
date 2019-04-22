0bcf6dfe88ba17d0ba3063921ce4ee530375fa4a
dienkim <trietnk01@gmail.com>
2019-04-22 08:47:11 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
+++ wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
@@ -6,12 +6,7 @@ add_action('init','p_load_support');
 function p_load_support(){
     register_nav_menus(
         array(
-            "primary"    => __( "Primay Menu"),
-            "related_product_menu" => __("Menu sản phẩm liên quan"), 
-            "menu_content_bottom_1" => __("menu_content_bottom_1"),       
-            "menu_content_bottom_2" => __("menu_content_bottom_2"),       
-            "menu_content_bottom_3" => __("menu_content_bottom_3"),       
-            "menu_content_bottom_4" => __("menu_content_bottom_4"),       
+            "primary"    => __( "Primay Menu"),            
         )
     );
 
