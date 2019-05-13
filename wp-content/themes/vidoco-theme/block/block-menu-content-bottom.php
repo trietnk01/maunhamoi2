bdfbdfc36fce78182b9a4d498e7798da8a41c3ff
dienkim <trietnk01@gmail.com>
2019-05-13 16:13:30 +0700

    cập nhật

--- wp-content/themes/vidoco-theme/block/block-menu-content-bottom.php
+++ wp-content/themes/vidoco-theme/block/block-menu-content-bottom.php
@@ -1,135 +1,110 @@
 <div class="row">
-						<div class="col-sm-3">
-							<div class="content-bottom">
-								<h3 class="content-bottom-h">maunhamoi.com.vn</h3>
-								<?php			
-							$args = array( 
-								'menu'              => '', 
-								'container'         => '', 
-								'container_class'   => '', 
-								'container_id'      => '', 
-								'menu_class'        => 'ul-content-bttm',                             
-								'echo'              => true, 
-								'fallback_cb'       => 'wp_page_menu', 
-								'before'            => '', 
-								'after'             => '', 
-								'link_before'       => '', 
-								'link_after'        => '', 
-								'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
-								'depth'             => 3, 
-								'walker'            => '', 
-								'theme_location'    => 'menu_content_bottom_1' ,
-								'menu_li_actived'       => 'current-menu-item',
-								'menu_item_has_children'=> 'menu-item-has-children',
-							);
-							wp_nav_menu($args);
-							?>        		
-								<!--<ul class="ul-content-bttm">
-									<li><a href="javascript:void(0);">Trang chủ</a></li>
-									<li><a href="javascript:void(0);">Giới thiệu</a></li>
-									<li><a href="javascript:void(0);">Liên hệ</a></li>
-								</ul>-->
-							</div>
-						</div>
-						<div class="col-sm-3">
-							<div class="content-bottom">
-								<h3 class="content-bottom-h">Mẫu thiết kế</h3>
-								<?php			
-							$args = array( 
-								'menu'              => '', 
-								'container'         => '', 
-								'container_class'   => '', 
-								'container_id'      => '', 
-								'menu_class'        => 'ul-content-bttm',                             
-								'echo'              => true, 
-								'fallback_cb'       => 'wp_page_menu', 
-								'before'            => '', 
-								'after'             => '', 
-								'link_before'       => '', 
-								'link_after'        => '', 
-								'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
-								'depth'             => 3, 
-								'walker'            => '', 
-								'theme_location'    => 'menu_content_bottom_2' ,
-								'menu_li_actived'       => 'current-menu-item',
-								'menu_item_has_children'=> 'menu-item-has-children',
-							);
-							wp_nav_menu($args);
-							?>        		
-								<!--<ul class="ul-content-bttm">
-									<li><a href="javascript:void(0);">Biệt thự đẹp</a></li>
-									<li><a href="javascript:void(0);">Nhà phố đẹp</a></li>
-									<li><a href="javascript:void(0);">Thiết kế nội thất</a></li>
-									<li><a href="javascript:void(0);">Sân vườn - Tiêu cảnh</a></li>
-									<li><a href="javascript:void(0);">Đã thi công</a></li>
-								</ul>-->
-							</div>
-						</div>
-						<div class="col-sm-3">
-							<div class="content-bottom">
-								<h3 class="content-bottom-h">Tạp chí</h3>
-								<?php			
-							$args = array( 
-								'menu'              => '', 
-								'container'         => '', 
-								'container_class'   => '', 
-								'container_id'      => '', 
-								'menu_class'        => 'ul-content-bttm',                             
-								'echo'              => true, 
-								'fallback_cb'       => 'wp_page_menu', 
-								'before'            => '', 
-								'after'             => '', 
-								'link_before'       => '', 
-								'link_after'        => '', 
-								'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
-								'depth'             => 3, 
-								'walker'            => '', 
-								'theme_location'    => 'menu_content_bottom_3' ,
-								'menu_li_actived'       => 'current-menu-item',
-								'menu_item_has_children'=> 'menu-item-has-children',
-							);
-							wp_nav_menu($args);
-							?>        		
-								<!--<ul class="ul-content-bttm">
-									<li><a href="javascript:void(0);">Tạp chí kiến trúc</a></li>
-									<li><a href="javascript:void(0);">Tin tức và sự kiện</a></li>
-									<li><a href="javascript:void(0);">Kiến trúc đẹp</a></li>
-									<li><a href="javascript:void(0);">Nội thất đẹp</a></li>
-									<li><a href="javascript:void(0);">Không gian đẹp</a></li>
-								</ul>-->
-							</div>
-						</div>
-						<div class="col-sm-3">
-							<div class="content-bottom">
-								<h3 class="content-bottom-h">Liên kết</h3>
-								<?php			
-							$args = array( 
-								'menu'              => '', 
-								'container'         => '', 
-								'container_class'   => '', 
-								'container_id'      => '', 
-								'menu_class'        => 'ul-content-bttm',                             
-								'echo'              => true, 
-								'fallback_cb'       => 'wp_page_menu', 
-								'before'            => '', 
-								'after'             => '', 
-								'link_before'       => '', 
-								'link_after'        => '', 
-								'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',  
-								'depth'             => 3, 
-								'walker'            => '', 
-								'theme_location'    => 'menu_content_bottom_4' ,
-								'menu_li_actived'       => 'current-menu-item',
-								'menu_item_has_children'=> 'menu-item-has-children',
-							);
-							wp_nav_menu($args);
-							?>        		
-								<!--<ul class="ul-content-bttm">
-									<li><a href="javascript:void(0);">Mỹ thuật ứng dụng</a></li>
-									<li><a href="javascript:void(0);">Nhà của sao</a></li>
-									<li><a href="javascript:void(0);">Thời trang</a></li>
-									<li><a href="javascript:void(0);">Người đẹp kiến trúc đẹp</a></li>
-								</ul>-->
-							</div>
-						</div>
-					</div>
+	<div class="col-sm-3">
+		<div class="content-bottom">
+			<h3 class="content-bottom-h">maunhamoi.com.vn</h3>
+			<?php
+			$args = array(
+				'menu'              => '',
+				'container'         => '',
+				'container_class'   => '',
+				'container_id'      => '',
+				'menu_class'        => 'ul-content-bttm',
+				'echo'              => true,
+				'fallback_cb'       => 'wp_page_menu',
+				'before'            => '',
+				'after'             => '',
+				'link_before'       => '',
+				'link_after'        => '',
+				'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'             => 3,
+				'walker'            => '',
+				'theme_location'    => 'menu_content_bottom_1' ,
+				'menu_li_actived'       => 'current-menu-item',
+				'menu_item_has_children'=> 'menu-item-has-children',
+			);
+			wp_nav_menu($args);
+			?>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="content-bottom">
+			<h3 class="content-bottom-h">Mẫu thiết kế nhà đẹp</h3>
+			<?php
+			$args = array(
+				'menu'              => '',
+				'container'         => '',
+				'container_class'   => '',
+				'container_id'      => '',
+				'menu_class'        => 'ul-content-bttm',
+				'echo'              => true,
+				'fallback_cb'       => 'wp_page_menu',
+				'before'            => '',
+				'after'             => '',
+				'link_before'       => '',
+				'link_after'        => '',
+				'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'             => 3,
+				'walker'            => '',
+				'theme_location'    => 'menu_content_bottom_2' ,
+				'menu_li_actived'       => 'current-menu-item',
+				'menu_item_has_children'=> 'menu-item-has-children',
+			);
+			wp_nav_menu($args);
+			?>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="content-bottom">
+			<h3 class="content-bottom-h">Mẫu thiết kế đẹp</h3>
+			<?php
+			$args = array(
+				'menu'              => '',
+				'container'         => '',
+				'container_class'   => '',
+				'container_id'      => '',
+				'menu_class'        => 'ul-content-bttm',
+				'echo'              => true,
+				'fallback_cb'       => 'wp_page_menu',
+				'before'            => '',
+				'after'             => '',
+				'link_before'       => '',
+				'link_after'        => '',
+				'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'             => 3,
+				'walker'            => '',
+				'theme_location'    => 'menu_content_bottom_3' ,
+				'menu_li_actived'       => 'current-menu-item',
+				'menu_item_has_children'=> 'menu-item-has-children',
+			);
+			wp_nav_menu($args);
+			?>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="content-bottom">
+			<h3 class="content-bottom-h">Tạp chí</h3>
+			<?php
+			$args = array(
+				'menu'              => '',
+				'container'         => '',
+				'container_class'   => '',
+				'container_id'      => '',
+				'menu_class'        => 'ul-content-bttm',
+				'echo'              => true,
+				'fallback_cb'       => 'wp_page_menu',
+				'before'            => '',
+				'after'             => '',
+				'link_before'       => '',
+				'link_after'        => '',
+				'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'             => 3,
+				'walker'            => '',
+				'theme_location'    => 'menu_content_bottom_4' ,
+				'menu_li_actived'       => 'current-menu-item',
+				'menu_item_has_children'=> 'menu-item-has-children',
+			);
+			wp_nav_menu($args);
+			?>
+		</div>
+	</div>
+</div>
--- wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
+++ wp-content/themes/vidoco-theme/inc/default/add-theme-support.php
@@ -6,7 +6,11 @@ add_action('init','p_load_support');
 function p_load_support(){
     register_nav_menus(
         array(
-            "primary"    => __( "Primay Menu"),            
+            "primary"    => __( "Primay Menu"),
+            "menu_content_bottom_1"=>("Menu content bottom 1"),
+            "menu_content_bottom_2"=>("Menu content bottom 2"),
+            "menu_content_bottom_3"=>("Menu content bottom 3"),
+            "menu_content_bottom_4"=>("Menu content bottom 4"),
         )
     );
 
